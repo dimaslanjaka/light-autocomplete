f5a22b6a8ae56a865750c324f8b901c81d2bc6fe
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-09 09:50:19 +0700

    add autoload

--- readme.md
+++ readme.md
@@ -4,5 +4,7 @@ simple input form auto complete
 
 ## Pre-requirements
 - wrapper of input must have style `position: relative`
+- `autocomplete=off` in input or textarea
+- `data-autocomplete='[json array]'` in input or textarea
 
 [initial project](https://codepen.io/dimaslanjaka/pen/MWxqJRX?editors=1010)
---
+++ src/autoload.ts
@@ -0,0 +1,13 @@
+import autocomplete from './autocomplete';
+
+(function () {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://github.com/dimaslanjaka/light-autocomplete/blob/master/src/style.css';
+  if (document.head) document.head.appendChild(link);
+
+  // init auto complete
+  document.querySelectorAll('[autocomplete=off]').forEach(input => {
+    autocomplete(input, JSON.parse(input.getAttribute('data-autocomplete') || '[]'));
+  });
+})();
