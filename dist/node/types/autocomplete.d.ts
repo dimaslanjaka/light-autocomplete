38876fd5eed90506c572dec913ca1820222631a3
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-09 11:31:56 +0700

    restore dist on `master`

---
+++ dist/node/types/autocomplete.d.ts
@@ -0,0 +1,6 @@
+/**
+ * autocomplete
+ * @param {HTMLElement|Element|HTMLInputElement|HTMLTextAreaElement|null} input
+ * @param {string[]} arrayData
+ */
+export default function autocomplete(input: HTMLElement | Element | HTMLInputElement | HTMLTextAreaElement | null, arrayData: string[]): void;
