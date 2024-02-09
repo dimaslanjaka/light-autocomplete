87a1876fef20b2ac054e1d72a79274e4170b8a7d
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-09 11:29:54 +0700

    ignore dist on `pre-release` folder

--- dist/types/findStr.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * search string from list
- * @param keyword string to search
- * @param dictionary search list
- * @param wildcard when disabled search only for keywrod start with. when enabled push all possible matches.
- */
-export default function findStr(keyword: string, dictionary: string[], wildcard?: boolean): string[];
